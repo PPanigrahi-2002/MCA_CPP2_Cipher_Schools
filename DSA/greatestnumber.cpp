797e841ad97bff59f3c104fb4104622595af6124
PPanigrahi-2002 <priyankapanigrahi04@gmail.com>
2024-06-19 14:26:50 +0530

    check

--- DSA/greatestnumber.cpp
+++ DSA/greatestnumber.cpp
@@ -12,10 +12,23 @@ int greatestnumber(int arr[], int size)
     }
     return max;
 }
+int minimumvalue(int arr[], int size)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
 int main()
 {
     // int n;
     // cin >> n;
     int arr[5] = {1, 2, 3, 4, 5};
-    cout << greatestnumber(arr, 5);
+    cout << greatestnumber(arr, 5) << endl;
+    cout << minimumvalue(arr, 5);
 }
