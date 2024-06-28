d7fbc95bd1c320aecf2312b31016116f5022f7cc
PPanigrahi-2002 <priyankapanigrahi04@gmail.com>
2024-06-28 15:37:01 +0530

    check

---
+++ DSA/searchrecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+bool isorted(int arr[], int size)
+{
+    if (size == 0 || size == 1)
+        return true;
+    if (arr[0] > arr[1])
+    {
+        return false;
+    }
+    else
+    {
+        return isorted(arr + 1, size - 1);
+    }
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << isorted(arr, 5);
+}
---
+++ DSA/sumrecursion.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int sumofarray(int arr[], int size)
+{
+    if (size == 0 || size == 1)
+        return
+}
