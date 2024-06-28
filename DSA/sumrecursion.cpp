379d3ba2ceaba54e5db25e18c7048d742fdb6387
PPanigrahi-2002 <priyankapanigrahi04@gmail.com>
2024-06-28 15:56:28 +0530

    check

--- DSA/sumrecursion.cpp
+++ DSA/sumrecursion.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int sumofarray(int arr[], int size)
 {
-    if (size == 0 || size == 1)
-        return
+    int sum = 0;
+    if (size == 0)
+        return 0;
+    if (size == 1)
+        return arr[0];
+    return arr[0] + sumofarray(arr + 1, size - 1);
+    // int remainingpart = sumarray(arr+1,size -1);
+    // int sum = arr[0]+ remaingpart;
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << sumofarray(arr, 5);
 }
