4c3c29aee5a4ff7d90be99bf0aa90623d23bc34e
PPanigrahi-2002 <priyankapanigrahi04@gmail.com>
2024-07-01 14:44:35 +0530

    check

---
+++ DSA/linkedlist.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+void insertattail(Node *&head, int value)
+{
+    Node *n = new Node(value);
+    Node *temp = head;
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void insertathead(Node *&head, int value)
+{
+    Node *n = new Node(value);
+
+    Node *temp = n;
+    temp->next = head;
+    head = n;
+}
+bool search(Node *head, int key)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+            return true;
+    }
+    return false;
+}
+void display(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    insertathead(head, 10);
+    insertathead(head, 101);
+    insertathead(head, 11);
+    display(head);
+    cout << search(head, 11);
+}
---
+++ DSA/ll.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+void insertattail(Node *head, int value)
+{
+    Node *n = new Node(value);
+    Node *temp = head;
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    insertattail(head, 10);
+    insertattail(head, 101);
+    insertattail(head, 11);
+    display(head);
+}
