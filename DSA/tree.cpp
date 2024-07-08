15358721f974a51aa7a7b29aebf15c1f23c8eacb
Priyanka Panigrahi <166863514+PPanigrahi-2002@users.noreply.github.com>
2024-07-08 15:37:53 +0530

    check

---
+++ DSA/tree.cpp
@@ -0,0 +1,27 @@
+// tree - a non linear data structure
+// Depth - number of edges between leaf to node
+// height = nuber of longest path between node and leaf
+// level 2^n;
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+}
