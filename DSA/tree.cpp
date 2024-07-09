224dc518525884d9e4b6a35a6f0364836faae393
Priyanka Panigrahi <166863514+PPanigrahi-2002@users.noreply.github.com>
2024-07-09 13:14:30 +0530

    check

--- DSA/tree.cpp
+++ DSA/tree.cpp
@@ -17,6 +17,31 @@ public:
         right = NULL;
     }
 };
+void preorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+
+    postorder(root->right);
+    cout << root->data << " ";
+}
 int main()
 {
     Node *root = new Node(1);
@@ -24,4 +49,9 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    preorder(root);
+    cout << endl;
+    inorder(root);
+    cout << endl;
+    postorder(root);
 }
