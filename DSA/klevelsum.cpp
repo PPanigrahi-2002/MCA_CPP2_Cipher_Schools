249953de6162cfda916826e28611dfed81d9c103
Priyanka Panigrahi <166863514+PPanigrahi-2002@users.noreply.github.com>
2024-07-09 14:33:26 +0530

    check

--- DSA/klevelsum.cpp
+++ DSA/klevelsum.cpp
@@ -1,12 +1,18 @@
+// create queue
+// push root and NULL
+// pop the front element and check if front ele is not null then then push the left and right ele of front till the q is not empty
+
 #include <iostream>
 #include <queue>
 using namespace std;
+
 class Node
 {
 public:
     int data;
     Node *left;
     Node *right;
+
     Node(int d)
     {
         data = d;
@@ -14,18 +20,24 @@ public:
         right = NULL;
     }
 };
-void printLevelorder(Node *root)
+
+void sumAtKthLevel(Node *root, int k)
 {
     queue<Node *> q;
     q.push(root);
     q.push(NULL);
+    int level, sum = 0;
     while (!q.empty())
     {
+
         Node *node = q.front();
         q.pop();
         if (node != NULL)
         {
-            cout << node->data << " ";
+            if (level == k)
+            {
+                sum += node->data;
+            }
             if (node->left)
             {
                 q.push(node->left);
@@ -35,18 +47,22 @@ void printLevelorder(Node *root)
                 q.push(node->right);
             }
         }
+
         else if (!q.empty())
         {
             q.push(NULL);
+            level++;
         }
     }
+    cout << "Level : " << level << " Sum : " << sum;
 }
 int main()
 {
     Node *root = new Node(1);
     root->left = new Node(2);
-    root->right = new Node(NULL);
+    root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    printLevelorder(root);
+
+    sumAtKthLevel(root, 2);
 }
